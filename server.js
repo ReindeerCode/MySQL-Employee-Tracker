27eb1fa0005379d7312953751ef8e2e54dce7201
MJRudolph <ReindeerCode@gmail.com>
2020-12-17 18:09:59 -0500

    removed bloat code

--- server.js
+++ server.js
@@ -28,35 +28,17 @@ connection.connect((err) => {
 });
 
 function starter() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "starterQuestion",
-        message: "What would you like to do?",
-        choices: [
-          "View departments.",
-          "View Roles.",
-          "View Employees.",
-          "Add department.",
-          "Add Role.",
-          "Add Employee.",
-          "Update an employee's role.",
-          "I'm done.",
-        ],
-      },
-    ])
-    .then((answers) => {
-      if (answers.starterQuestion === "View departments.") {
-        getDepartment();
-      } else if (answers.starterQuestion === "View Roles.") {
-        getRoles();
-      } else if (answers.starterQuestion === "View Employees.") {
-        getEmployees();
-      } else {
-        connection.end();
-      }
-    });
+  inquirer.prompt(questions.starterQ).then((answers) => {
+    if (answers.starterQuestion === "View departments.") {
+      getDepartment();
+    } else if (answers.starterQuestion === "View Roles.") {
+      getRoles();
+    } else if (answers.starterQuestion === "View Employees.") {
+      getEmployees();
+    } else {
+      connection.end();
+    }
+  });
 }
 
 function getDepartment() {
@@ -95,73 +77,6 @@ function getEmployees() {
   });
 }
 
-// function addNewDepartName(answers) {
-//   connection.query(
-//     `USE employee_trackerDB;
-//     insert into department(name)
-//     value ("${answers.newDepartName}");`,
-//     function (err, res) {
-//       if (err) throw err;
-//       console.table(res);
-//     }
-//   );
-// }
-// function addNewRoleInfo(answers) {
-//   connection.query(
-//     `USE employee_trackerDB;
-//     insert into role(title, salary, department_id)
-//     value ("${answers.newRoleTitle}","${answers.newRoleSalary}", "${answers.newRoleDepartment_ID}");`,
-//     function (err, res) {
-//       if (err) throw err;
-//       console.table(res);
-//     }
-//   );
-// }
-
-// function addNewEmployee(answers) {
-//   connection.query(
-//     `USE employee_trackerDB;
-//     insert into employee(first_name, last_name, manager_id)
-//     value ("${answers.newFirstName}","${answers.newLastName}", "${answers.newEmployeeID}", "${answers.newEmployeeManager_ID}");`,
-//     function (err, res) {
-//       if (err) throw err;
-//       console.table(res);
-//     }
-//   );
-// }
-//end user wants to add to tables function___________________________
-
-//user wants to view table data function___________________________
-// function view () {
-// inquirer.prompt(questions.view).then((answers) => {
-//     if (answers.viewSomething === "View departments.") {
-//     //   console.table(`SELECT * FROM department`);
-//     } else if (
-//       answers.viewSomething === "View roles."
-//     ) {
-//       // console.table(`select * from role`);
-//     } else if (answers.viewSomething === "View employees.") {
-//       // console.table(`select * from employee`);
-//     } else {
-//       afterConnection();
-//     }
-//   });
-// }
-//end user wants to view table data function___________________________
-
-//user wants to update role table data function
-// function update () {
-//   inquirer.prompt(questions.update).then((answers) => {
-//     const update = new Update(
-//       answers.employee,
-//     )
-//   });
-//  tableUpdates.push(update);
-//  //call next function here
-// }
-//end user wants to update role table data function_______________________
-//end inquirer questions functions___________________________
-
 function afterConnection() {
   connection.query(
     `select * from employee
@@ -173,9 +88,6 @@ function afterConnection() {
     }
   );
 }
-// // initialize starter function
-// starter();
-// // end initialize starter function
 
 // listen
 app.listen(PORT, function () {
